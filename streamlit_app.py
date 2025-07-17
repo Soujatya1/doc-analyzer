c73b376aa2dcbb0938817aac58d1a82161df2b75
Soujatya Das <soujatya548@gmail.com>
2025-07-17 09:35:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,6 +112,8 @@ Your task is to generate a **clean, section-wise and chapter-wise summary** of t
 3. Do not miss out on any tabular data pointers
 4. There will be header stating with "Definitions or Definitions applicable to Part A of these Regulations" in the source file, which needs to be correctly captured in the generated response
 5. Under the header, there will be a short write-up which also needs to be captured, starting with headers like "F. No. IRDAI/Reg/11/205/2024 or G.S.R. 02(E)"
+6. Capture the core essence of each pointer and respond as per
+7. Please do not miss out on any pointers under any header
 
 ---
 
